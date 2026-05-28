2c0a1237d3f340725a9efa5c15e5e4e793b89e7a
iefnaug <guanfei1993@gmail.com>
2026-05-28 14:47:10 +0800

    条件渲染

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Demo01 from "./components/Demo01.vue";
 import Demo02 from "./components/Demo02.vue";
 // import Demo03 from "./components/Demo03.vue";
-import Demo04 from "./components/Demo04.vue";
+// import Demo04 from "./components/Demo04.vue";
+import Demo05 from "./components/Demo05.vue";
 </script>
 
 <template>
 <!--  <Demo01></Demo01>-->
 <!--  <Demo02></Demo02>-->
 <!--  <Demo03></Demo03>-->
-  <Demo04></Demo04>
+<!--  <Demo04></Demo04>-->
+  <Demo05></Demo05>
 </template>
---
+++ src/components/Demo05.vue
@@ -0,0 +1,29 @@
+<script setup>
+//条件渲染
+import {ref} from "vue";
+
+const show = ref(false)
+function showOrHide() {
+  show.value = !show.value;
+}
+</script>
+
+<template>
+  <button @click="showOrHide">click me</button>
+  <div class="box" v-if="show">1</div>
+  <template v-else>
+    <div class="box">2</div>
+    <div class="box">3</div>
+  </template>
+
+  <h1 v-show="true">v-show</h1>
+
+</template>
+
+<style scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: #c084fc;
+}
+</style>
