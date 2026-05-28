fd62631f6dd27b5d68aba3e859240f513d4c98bc
iefnaug <guanfei1993@gmail.com>
2026-05-28 11:07:03 +0800

    样式绑定

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Demo01 from "./components/Demo01.vue";
 import Demo02 from "./components/Demo02.vue";
-import Demo03 from "./components/Demo03.vue";
+// import Demo03 from "./components/Demo03.vue";
+import Demo04 from "./components/Demo04.vue";
 </script>
 
 <template>
 <!--  <Demo01></Demo01>-->
 <!--  <Demo02></Demo02>-->
-  <Demo03></Demo03>
+<!--  <Demo03></Demo03>-->
+  <Demo04></Demo04>
 </template>
---
+++ src/components/Demo04.vue
@@ -0,0 +1,56 @@
+<script setup>
+//类与样式绑定
+import {computed, reactive, ref} from 'vue'
+const isActive = ref(true)
+//响应式
+// const multi = reactive({
+//   s1: true,
+//   s2: true
+// })
+
+//计算属性
+const multi = computed(() => {
+  return {
+    s1: Date.now() > 0,
+    s2: true
+  }
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <!--在原有属性的机上可以叠加-->
+    <div class="box" :class="{active: isActive}"></div>
+    <div :class="multi"></div>
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+}
+.wrapper div {
+  flex: 1;
+  background-color: #c084fc;
+}
+
+.wrapper .box {
+  border: 2px solid white;
+}
+
+.wrapper .s1 {
+  border: 2px solid green;
+}
+
+.wrapper .s2 {
+  border-radius: 50%;
+}
+
+.wrapper .active {
+  background-color: lightsalmon;
+}
+</style>
