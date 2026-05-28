17e86a7e7047086b2f0d91d4f4d0b34a0ee0ceca
iefnaug <guanfei1993@gmail.com>
2026-05-28 15:57:23 +0800

    循环

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import Demo01 from "./components/Demo01.vue";
 import Demo02 from "./components/Demo02.vue";
 // import Demo03 from "./components/Demo03.vue";
 // import Demo04 from "./components/Demo04.vue";
-import Demo05 from "./components/Demo05.vue";
+// import Demo05 from "./components/Demo05.vue";
+import Demo06 from "./components/Demo06.vue";
 </script>
 
 <template>
@@ -11,5 +12,6 @@ import Demo05 from "./components/Demo05.vue";
 <!--  <Demo02></Demo02>-->
 <!--  <Demo03></Demo03>-->
 <!--  <Demo04></Demo04>-->
-  <Demo05></Demo05>
+<!--  <Demo05></Demo05>-->
+  <Demo06></Demo06>
 </template>
---
+++ src/components/Demo06.vue
@@ -0,0 +1,49 @@
+<script setup>
+import {ref} from "vue";
+
+const messages = ref([
+  {message: "haha"},
+  {message: "haha2"}
+])
+
+const obj = ref({
+  name: "John",
+  age: 30
+})
+
+function add() {
+  messages.value.push({
+    message: "hahaN" + Math.random(),
+  })
+}
+
+function reset() {
+  messages.value = [];
+}
+</script>
+
+<template>
+  <button @click="add">add</button>
+  <button @click="reset">reset</button>
+  <div>
+    <ul>
+      <li v-for="(item, index) in messages">{{ index }}: {{ item.message }}</li>
+    </ul>
+
+    <ul>
+      <li v-for="({message}, index) in messages">{{ index }}: {{ message }}</li>
+    </ul>
+  </div>
+
+  <div>
+    <template v-for="(v, k, idx) in obj" :key="idx">
+      <div>
+        <span>{{ k }}: {{ v }} : {{ idx }}</span>
+      </div>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
