60440dd66fd9cde886eb1a869eea7c98a89928b7
iefnaug <guanfei1993@gmail.com>
2026-06-06 13:13:29 +0800

    表单绑定

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import Demo02 from "./components/Demo02.vue";
 // import Demo03 from "./components/Demo03.vue";
 // import Demo04 from "./components/Demo04.vue";
 // import Demo05 from "./components/Demo05.vue";
-import Demo06 from "./components/Demo06.vue";
+// import Demo06 from "./components/Demo06.vue";
+import Demo07 from "./components/Demo07.vue";
 </script>
 
 <template>
@@ -13,5 +14,6 @@ import Demo06 from "./components/Demo06.vue";
 <!--  <Demo03></Demo03>-->
 <!--  <Demo04></Demo04>-->
 <!--  <Demo05></Demo05>-->
-  <Demo06></Demo06>
+  <!-- <Demo06></Demo06> -->
+   <Demo07></Demo07>
 </template>
---
+++ src/components/Demo07.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { reactive } from "vue";
+const user = reactive({
+    name: '张三',
+    age: 18,
+    hobby: ['篮球'],
+    city: '北京',
+    gender: '男',
+});
+
+function submitForm() {
+    console.log(user);
+    // 在这里处理表单提交逻辑，如发送API请求
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="submitForm">
+      <input type="text" v-model="user.name" placeholder="请输入姓名"><br>
+      <input type="number" v-model="user.age" placeholder="请输入年龄"><br>
+      性别
+      <input type="radio" v-model="user.gender" value="男">男
+      <input type="radio" v-model="user.gender" value="女">女
+      <br>
+      爱好
+      <input type="checkbox" v-model="user.hobby" value="篮球">篮球
+      <input type="checkbox" v-model="user.hobby" value="足球">足球
+      <input type="checkbox" v-model="user.hobby" value="跑步">跑步
+      <input type="checkbox" v-model="user.hobby" value="其他">其他
+      <br>
+      城市
+      <select v-model="user.city">
+        <option value="北京">北京</option>
+        <option value="上海">上海</option>
+        <option value="广州">广州</option>
+        <option value="深圳">深圳</option>
+      </select><br>
+      <button type="submit">提交</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    background-color: #c084fc;
+  }
+</style>
