e23aebe5fc8985ec9bbed181f6263bf50f0f6fd3
iefnaug <guanfei1993@gmail.com>
2026-07-14 14:55:59 +0800

    example: 属性绑定

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,11 @@
 // import Demo07 from "./components/Demo07.vue";
 // import Demo08 from "./components/Demo08.vue";
 // import Demo09 from "./components/basic/Demo09.vue";
-import Demo10 from "./components/basic/Demo10.vue";
+// import Demo10 from "./components/basic/Demo10.vue";
+import Demo10 from "./components/examples/basic/HandleUserInput.vue";
+import HandleUserInput from "./components/examples/basic/HandleUserInput.vue";
+import At from "./components/examples/basic/AttributeBinding.vue";
+import AttributeBinding from "./components/examples/basic/AttributeBinding.vue";
 </script>
 
 <template>
@@ -21,5 +25,7 @@ import Demo10 from "./components/basic/Demo10.vue";
 <!--   <Demo07></Demo07>-->
   <!-- <Demo08></Demo08> -->
 <!--  <Demo09></Demo09>-->
-  <Demo10></Demo10>
+<!--  <Demo10></Demo10>-->
+<!--  <HandleUserInput></HandleUserInput>-->
+  <AttributeBinding></AttributeBinding>
 </template>
